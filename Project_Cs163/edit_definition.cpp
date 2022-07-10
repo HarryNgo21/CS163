a6f1debc3d7bc6335f4a52b91543d62f86cfddc4
phungptn <ngothaiphung412@gmail.com>
2022-07-10 19:26:21 +0700

    Create edit_definition.cpp

---
+++ Project_Cs163/edit_definition.cpp
@@ -0,0 +1,14 @@
+#include "edit_definition.h"
+
+//void edit_definition(wstring word, wstring def, AVL tree) {
+//	/*wstring new_def = L"";
+//	wcout << L"Enter new definition: ";
+//	getline(wcin, new_def);
+//	if (new_def == L"") {
+//		wcout << L"No change made." << endl;
+//		return;
+//	}
+//	tree.remove(word);
+//	tree.insert(word, new_def);
+//	wcout << L"Definition changed." << endl;*/
+//}
