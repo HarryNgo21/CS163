58b061571fc0770d4ee1b747c33b3d37c648b917
phungptn <ngothaiphung412@gmail.com>
2022-07-12 00:19:00 +0700

    Update Add.cpp

--- Project_Cs163/Add.cpp
+++ Project_Cs163/Add.cpp
@@ -7,7 +7,7 @@ int AVL::Add(AVL& tree, wstring key, wstring def, string def_dir)
 	ofstream fout(def_dir, ios_base::binary | ios_base::app | ios_base::ate);
 	if (!fout.is_open()) return -2;
 	int d = fout.tellp(),l = def.length()+1;
-	int numb = 0;
+	int numb = 1;
 	fout.write((char*)&numb, sizeof(int));
 	fout.write((char*)&l, sizeof(int));
 	fout.write((char*)&def[0], l * sizeof(wchar_t));
