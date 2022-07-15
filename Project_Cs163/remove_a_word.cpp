77c769278d748829487bbf61e27c2e7c8f86231a
phungptn <ngothaiphung412@gmail.com>
2022-07-15 10:19:20 +0700

    Update remove_a_word.cpp

--- Project_Cs163/remove_a_word.cpp
+++ Project_Cs163/remove_a_word.cpp
@@ -1,9 +1,9 @@
 
 #include "remove_a_word.h"
 
-bool remove_def_from_file(int d, wstring word_dir) {
+bool remove_def_from_file(int d, string def_dir) {
 	ifstream fin;
-	fin.open(word_dir, ios::binary | ios::in);
+	fin.open(def_dir, ios::binary | ios::in);
 	if (!fin.is_open()) {
 		return false;
 	}
@@ -12,29 +12,29 @@ bool remove_def_from_file(int d, wstring word_dir) {
 	fin.read((char*)&n, sizeof(int));
 	int* len = new int[n];
 	int size_delete = n;
-	for (int i = 0; i < n; ++i) { 
+	for (int i = 0; i < n; ++i) {
 		fin.read((char*)&len[i], sizeof(int));
 		fin.seekg(len[i]);
 		size_delete = size_delete + sizeof(int) + len[i];
-	}	
+	}
 	fin.close();
-	
+
 	ofstream fout;
-	fout.open(word_dir, ios::binary | ios::app | ios::out);
+	fout.open(def_dir, ios::binary | ios::out);
 	if (!fout.is_open()) {
 		return false;
 	}
 	fout.seekp(d);
 	for (int i = 0; i < size_delete; ++i) {
 		char del = ' ';
-
+		fout.write((char*)&del, sizeof(char));
 	}
 	fout.close();
+	return true;
 }
 
 bool remove_a_word(wstring word, AVL& tree, string def_dir) {
 	int d = tree.search(word)->d;
-	wstring word_dir =	wstring(def_dir.begin(), def_dir.end()) + (wchar_t)towupper(word[0]) + L".bin";
-
+	remove_def_from_file(d, def_dir);
 	return tree.remove(word);
 }
